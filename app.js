f9da2dc9dd5a97165d024409d906fb53d65772c5
billbunkum <william.richard.hurst@gmail.com>
2016-12-14 22:20:29 -0500

    init angular

---
+++ app.js
@@ -0,0 +1,14 @@
+angular.module('root', [])
+    .controller('RootCtrl', RootController);
+
+function RootController() {
+    const ctrl = this;
+
+// controller SCALARS
+    ctrl.doit = false;
+
+// controller FUNCTIONS
+
+
+// defined FUNCTIONS 
+}
