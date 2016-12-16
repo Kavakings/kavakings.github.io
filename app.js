473b2ca6ec4fbddb4b8749ece9727c6580964d78
billbunkum <william.richard.hurst@gmail.com>
2016-12-15 19:40:36 -0500

    js funct for getting files doesn't work

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const app = angular.module("root", []).controller("RootCtrl", RootController);
 
-function RootController() {
+function RootController($http) {
     const ctrl = this;
 
 // controller SCALARS
-
 
 // controller FUNCTIONS
 // NAV functions
@@ -13,11 +12,21 @@ function RootController() {
     }
 
 // JUMBOTRON functions
+// file structure for kavakings.com/cpanel is as follows: //kavakings.com/imageSlideShow/
+    function getImageUrls() {
+        image_urls = [];
+        image_urls = $http.get("http://kavakings.com/imageSlideShow/")
+            .then(
+                console.log(image_urls)
+            );
+    }
+
     function carousel() {
-        
+        getImageUrls();
     }
 
 // defined FUNCTIONS 
     ctrl.orderLink = orderLink;
+    ctrl.getImageUrls = getImageUrls;
     ctrl.carousel = carousel;
 }
