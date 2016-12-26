0edc202abca40157dbd80865422a78ad9c0509e5
billbunkum <william.richard.hurst@gmail.com>
2016-12-26 14:21:42 -0500

    py file generates data.txt with image files as json data

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-import readDir from "./_media/";
-
-const app = angular.module("root", []).controller("RootCtrl", RootController);
+const app = angular.module("root", [])
+    .controller("RootCtrl", RootController);
 
 function RootController($http) {
     const ctrl = this;
@@ -9,28 +8,9 @@ function RootController($http) {
     ctrl.carousel = [];
 
 // controller FUNCTIONS
-
-    // idea from STACKOVERFLOW
-    // function _getAllFilesFromFolder(dir) {
-    //     let filesystem = require("fs");
-    //     let results = [];
-    //     filesystem.readdirSync(dir).forEach(function(file) {
-    //         file = dir+'/'+file;
-    //         let stat = filesystem.statSync(file);
-    //         if (stat && stat.isDirectory()) {
-    //             results = results.concat(_getAllFilesFromFolder(file))
-    //         } else results.push(file);
-    //     });
-    //     return results;
-    // };
-
-// function to put file names from _media into ctrl.carousel
-    //
-    // might try HARD-CODING in root dir to see if works
-    //
     function getCarousel() {
         let i = 0;
-        for (i = 0; i < readDir.files.length; i++){
+        for (i = 0; i < length; i++){
             console.log("yo");
         }
     }
@@ -40,14 +20,8 @@ function RootController($http) {
         alert("yeah!");
     }
 
-// JUMBOTRON functions
-// file structure for kavakings.com/cpanel is as follows: //kavakings.com/imageSlideShow/
-    // function getImageUrls() {
-    //     image_urls = [];
-    // }
-
 // ON-LOAD Functions
-    getCarousel();
+    // getCarousel();
 
 // defined FUNCTIONS 
     // ctrl._getAllFilesFromFolder = _getAllFilesFromFolder;
--- read_dir.py
+++ read_dir.py
@@ -1,7 +1,21 @@
 from os import listdir
 from os.path import isfile, join
+import json
 
-def readDir():
-    mypath = "./_media"
-    files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    return files
+class CollectImagePaths(object):
+    def readDir(self):
+        dir_path = "./_media"
+        self.image_files = [file for file in listdir(dir_path) if isfile(join(dir_path, file))]
+        return self.image_files
+
+    def makeJson(self):
+        data = self.image_files
+        with open('data.txt', 'w+') as self.outfile:
+            json.dump(data, self.outfile)
+
+    def execAll(self):
+        get_files = self.readDir()
+        self.makeJson()
+
+starterUp = CollectImagePaths()
+starterUp.execAll()
